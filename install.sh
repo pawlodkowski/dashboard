d2868f050e6ab05cacbcfdcff735769d2b650a84
Paul Wlodkowski <PaulWlodkowski@Pauls-MacBook-Pro-2.local>
2018-11-27 15:34:59 +0100

    modified install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/bash
 #
-# To be exectued on a RedHat EC2 instance with nothing
+# To be executed on a RedHat EC2 instance with nothing
 #
 
-# install Python, Django, git
+#install Python, Django, git
 sudo yum install python36
 sudo yum install python3-pip
 sudo yum install git
 
-# install the Python program
-git clone git@github.com:pawlodkowski/dashboard.git
-#assuming you have SSH; otherwise use HTTPs
-cd dashboard
-#pip3 install -r requirements.txt
-pip install --user -r requirements.txt
+#install python program
+# git clone git@github.com:pawlodkowski/dashboard.git
+#assuming we have SSH keys, otherwise use HTTPS
+git clone https://github.com/pawlodkowski/dashboard.git
 
-# download CSV file
+cd dashboard
+sudo pip3 install -r requirements.txt
+
+#download the csv file
 # curl http://somewhere.com/my.csv
-#comment out for now
