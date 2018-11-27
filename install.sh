b65d41412395e07edc64726dd7d1f5461c8bafc6
Paul Wlodkowski <PaulWlodkowski@Pauls-MacBook-Pro-2.local>
2018-11-27 13:26:19 +0100

    modified install.sh

--- install.sh
+++ install.sh
@@ -9,9 +9,11 @@ sudo yum install python3-pip
 sudo yum install git
 
 # install the Python program
-git clone github.com//myawesomedashboard.git
-cd myawesomedashboard-master
+git clone git@github.com:pawlodkowski/dashboard.git
+#assuming you have SSH; otherwise use HTTPs
+cd dashboard
 pip3 install -r requirements.txt
 
 # download CSV file
-curl http://somewhere.com/my.csv
+# curl http://somewhere.com/my.csv
+#comment out for now
